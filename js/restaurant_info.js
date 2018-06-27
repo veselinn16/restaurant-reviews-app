4b3285842526a8cde8120da2d3da74def0866707
veselin <veselinn16@yahoo.com>
2018-06-27 12:15:30 +0300

    Refactor restaurant view

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoidmVzZWwtb3V0IiwiYSI6ImNqaXZnaDExeTJrcTEzd3BhOXhjYmxwajkifQ.131KHjl660lf66ahdYc6tw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
