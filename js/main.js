fd30f79c44144e0bcb50ba86c4e50c3202709c72
Veselin Tonev <veselinn16@yahoo.com>
2019-06-11 14:15:00 +0300

    Update service worker code :recycle:

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-let restaurants,
-  neighborhoods,
-  cuisines
-var newMap
-var markers = []
+let restaurants, neighborhoods, cuisines;
+var newMap;
+var markers = [];
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("../sw.js")
+      .then(function(reg) {
+        console.log(
+          `Service Worker Registered Successfully with scope: ${reg.scope}`
+        );
+      })
+      .catch(function(err) {
+        console.log(`Failed to register service worker: ${err}!`);
+      });
+  });
+}
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
-document.addEventListener('DOMContentLoaded', (event) => {
-  initMap(); // added 
+document.addEventListener("DOMContentLoaded", event => {
+  initMap(); // added
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -18,78 +31,85 @@ document.addEventListener('DOMContentLoaded', (event) => {
  */
 fetchNeighborhoods = () => {
   DBHelper.fetchNeighborhoods((error, neighborhoods) => {
-    if (error) { // Got an error
+    if (error) {
+      // Got an error
       console.error(error);
     } else {
       self.neighborhoods = neighborhoods;
       fillNeighborhoodsHTML();
     }
   });
-}
+};
 
 /**
  * Set neighborhoods HTML.
  */
 fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
-  const select = document.getElementById('neighborhoods-select');
+  const select = document.getElementById("neighborhoods-select");
   neighborhoods.forEach(neighborhood => {
-    const option = document.createElement('option');
+    const option = document.createElement("option");
     option.innerHTML = neighborhood;
     option.value = neighborhood;
     select.append(option);
   });
-}
+};
 
 /**
  * Fetch all cuisines and set their HTML.
  */
 fetchCuisines = () => {
   DBHelper.fetchCuisines((error, cuisines) => {
-    if (error) { // Got an error!
+    if (error) {
+      // Got an error!
       console.error(error);
     } else {
       self.cuisines = cuisines;
       fillCuisinesHTML();
     }
   });
-}
+};
 
 /**
  * Set cuisines HTML.
  */
 fillCuisinesHTML = (cuisines = self.cuisines) => {
-  const select = document.getElementById('cuisines-select');
+  const select = document.getElementById("cuisines-select");
 
   cuisines.forEach(cuisine => {
-    const option = document.createElement('option');
+    const option = document.createElement("option");
     option.innerHTML = cuisine;
     option.value = cuisine;
     select.append(option);
   });
-}
+};
 
 /**
  * Initialize leaflet map, called from HTML.
  */
 initMap = () => {
-  self.newMap = L.map('map', {
-        center: [40.722216, -73.987501],
-        zoom: 12,
-        scrollWheelZoom: false
-      });
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoidmVzZWwtb3V0IiwiYSI6ImNqaXZnaDExeTJrcTEzd3BhOXhjYmxwajkifQ.131KHjl660lf66ahdYc6tw',
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-  }).addTo(newMap);
+  self.newMap = L.map("map", {
+    center: [40.722216, -73.987501],
+    zoom: 12,
+    scrollWheelZoom: false
+  });
+  L.tileLayer(
+    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}",
+    {
+      mapboxToken:
+        "pk.eyJ1IjoidmVzZWwtb3V0IiwiYSI6ImNqaXZnaDExeTJrcTEzd3BhOXhjYmxwajkifQ.131KHjl660lf66ahdYc6tw",
+      maxZoom: 18,
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: "mapbox.streets"
+    }
+  ).addTo(newMap);
 
-  document.getElementById('map').setAttribute('tabindex', '-1');
+  document.getElementById("map").setAttribute("tabindex", "-1");
 
   updateRestaurants();
-}
+};
 /* window.initMap = () => {
   let loc = {
     lat: 40.722216,
@@ -106,9 +126,9 @@ initMap = () => {
 /**
  * Update page and map for current restaurants.
  */
-updateRestaurants = () => { 
-  const cSelect = document.getElementById('cuisines-select');
-  const nSelect = document.getElementById('neighborhoods-select');
+updateRestaurants = () => {
+  const cSelect = document.getElementById("cuisines-select");
+  const nSelect = document.getElementById("neighborhoods-select");
 
   const cIndex = cSelect.selectedIndex;
   const nIndex = nSelect.selectedIndex;
@@ -116,24 +136,29 @@ updateRestaurants = () => {
   const cuisine = cSelect[cIndex].value;
   const neighborhood = nSelect[nIndex].value;
 
-  DBHelper.fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, (error, restaurants) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      resetRestaurants(restaurants);
-      fillRestaurantsHTML();
+  DBHelper.fetchRestaurantByCuisineAndNeighborhood(
+    cuisine,
+    neighborhood,
+    (error, restaurants) => {
+      if (error) {
+        // Got an error!
+        console.error(error);
+      } else {
+        resetRestaurants(restaurants);
+        fillRestaurantsHTML();
+      }
     }
-  })
-}
+  );
+};
 
 /**
  * Clear current restaurants, their HTML and remove their map markers.
  */
-resetRestaurants = (restaurants) => {
+resetRestaurants = restaurants => {
   // Remove all restaurants
   self.restaurants = [];
-  const ul = document.getElementById('restaurants-list');
-  ul.innerHTML = '';
+  const ul = document.getElementById("restaurants-list");
+  ul.innerHTML = "";
 
   // Remove all map markers
   if (self.markers) {
@@ -141,52 +166,55 @@ resetRestaurants = (restaurants) => {
   }
   self.markers = [];
   self.restaurants = restaurants;
-}
+};
 
 /**
  * Create all restaurants HTML and add them to the webpage.
  */
 fillRestaurantsHTML = (restaurants = self.restaurants) => {
-  const ul = document.getElementById('restaurants-list');
+  const ul = document.getElementById("restaurants-list");
   restaurants.forEach(restaurant => {
     ul.append(createRestaurantHTML(restaurant));
   });
   addMarkersToMap();
-}
+};
 
 /**
  * Create restaurant HTML.
  */
-createRestaurantHTML = (restaurant) => {
-  const li = document.createElement('li');
+createRestaurantHTML = restaurant => {
+  const li = document.createElement("li");
 
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  image.setAttribute('alt', `Picture of the ${restaurant.name} restaurant`);
+  const image = document.createElement("img");
+  image.className = "restaurant-img";
+  image.setAttribute("alt", `Picture of the ${restaurant.name} restaurant`);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement("h1");
   name.innerHTML = restaurant.name;
   li.append(name);
 
-  const neighborhood = document.createElement('p');
+  const neighborhood = document.createElement("p");
   neighborhood.innerHTML = restaurant.neighborhood;
   li.append(neighborhood);
 
-  const address = document.createElement('p');
+  const address = document.createElement("p");
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('button');
-  more.setAttribute('role', 'button');
-  more.setAttribute("onclick", `location.href='${DBHelper.urlForRestaurant(restaurant)}'`);
+  const more = document.createElement("button");
+  more.setAttribute("role", "button");
+  more.setAttribute(
+    "onclick",
+    `location.href='${DBHelper.urlForRestaurant(restaurant)}'`
+  );
   more.innerHTML = `View Details`;
   // more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.append(more);
 
-  return li
-}
+  return li;
+};
 
 /**
  * Add markers for current restaurants to the map.
@@ -201,8 +229,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     }
     self.markers.push(marker);
   });
-
-} 
+};
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
@@ -214,20 +241,20 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 } */
 
-const cSelect = document.getElementById('cuisines-select');
-const nSelect = document.getElementById('neighborhoods-select');
+const cSelect = document.getElementById("cuisines-select");
+const nSelect = document.getElementById("neighborhoods-select");
 
-nSelect.addEventListener('click', function() {
-  if(this.getAttribute('aria-expanded') === 'false') {
-    this.setAttribute('aria-expanded', 'true');
+nSelect.addEventListener("click", function() {
+  if (this.getAttribute("aria-expanded") === "false") {
+    this.setAttribute("aria-expanded", "true");
   } else {
     this.setAttribute("aria-expanded", "false");
   }
 });
 
-cSelect.addEventListener('click', function () {
-  if (this.getAttribute('aria-expanded') === 'false') {
-    this.setAttribute('aria-expanded', 'true');
+cSelect.addEventListener("click", function() {
+  if (this.getAttribute("aria-expanded") === "false") {
+    this.setAttribute("aria-expanded", "true");
   } else {
     this.setAttribute("aria-expanded", "false");
   }
--- sw.js
+++ sw.js
@@ -1,58 +1,61 @@
-let nameOfCache = 'restaurants-reviews-v1.0';
+const NAME_OF_CACHE = "restaurants-reviews-v1.0";
+let assets = [
+  "/index.html",
+  "/restaurant.html",
+  "js/main.js",
+  "js/dbhelper.js",
+  "js/restaurant_info.js",
+  "css/styles.css",
+  "css/media_queries.css",
+  "data/restaurants.json",
+  "img/1.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+  "img/4.jpg",
+  "img/5.jpg",
+  "img/6.jpg",
+  "img/7.jpg",
+  "img/8.jpg",
+  "img/9.jpg",
+  "img/10.jpg"
+];
 
-self.addEventListener('install', function (event) {
-    console.log('Worker install event in progress.')
-    event.waitUntil(
-        caches.open(nameOfCache).then(function (cache) {
-            return cache.addAll([
-                '/index.html',
-                '/restaurant.html',
-                'js/main.js',
-                'js/dbhelper.js',
-                'js/restaurant_info.js',
-                'css/styles.css',
-                'css/media_queries.css',
-                'img/1.jpg',
-                'img/2.jpg',
-                'img/3.jpg',
-                'img/4.jpg',
-                'img/5.jpg',
-                'img/6.jpg',
-                'img/7.jpg',
-                'img/8.jpg',
-                'img/9.jpg',
-                'img/10.jpg'
-            ]);
-        })
-            .then(function () {
-                console.log('Installation complete')
-            }).catch(function() {
-                console.log('Cache install failed!')
-            })
-    );
+self.addEventListener("install", function(event) {
+  console.log("Worker install event in progress.");
+  event.waitUntil(
+    caches
+      .open(NAME_OF_CACHE)
+      .then(function(cache) {
+        return cache.addAll(assets);
+      })
+      .then(function() {
+        console.log("Installation complete");
+      })
+      .catch(function() {
+        console.log("Cache install failed!");
+      })
+  );
 });
 
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function (cacheName) {
-                    return cacheName.startsWith('restaurants-reviews') &&
-                        cacheName != nameOfCache;
-                }).map(function (cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if ([NAME_OF_CACHE].indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    let requestUrl = new URL(event.request.url);
-
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
-})
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // if there is such a response in the cache, return it or perform the fetch request normally
+      return response || fetch(event.request);
+    })
+  );
+});
